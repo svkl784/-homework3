c12197686eed2200311a3be36223cb0910a22c69
Nataliya Svirid <klybiksvirid@gmail.com>
2021-01-30 19:06:40 +0300

    The change task2

--- src/Task2.java
+++ src/Task2.java
@@ -8,16 +8,17 @@ public class Task2 {
     private static final int MIN_BOUND = 0;
 
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("String: ");
+        StringBuilder string = new StringBuilder("String: ");
         for (int i = 0; i < 100; i++) {
             int stringInt = RANDOM.nextInt((MAX_BOUND - MIN_BOUND) + 1) + MIN_BOUND;
-            sb.append(stringInt).append(" ");
+            string.append(stringInt).append(" ");
         }
-        System.out.println(sb);
+        String newString = string.toString();
+        System.out.println(newString);
         System.out.print("New ");
         String z = " -1 ";
         Pattern pattern = Pattern.compile("\\s\\d\\d\\s");
-        Matcher matcher = pattern.matcher(sb);
+        Matcher matcher = pattern.matcher(string);
         String newSb = matcher.replaceAll(z);
         System.out.println(newSb);
     }
